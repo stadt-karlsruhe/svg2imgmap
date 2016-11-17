5cc3b37b2a6b13c1cc41b02fc819f33b32dca386
Florian Brucker <florian.brucker@mb.karlsruhe.de>
2016-11-17 15:34:18 +0100

    Setuptools infrastructure

---
+++ README.md
@@ -0,0 +1,41 @@
+# Create HTML image maps from SVG images
+
+## Installation
+
+    pip install -e git+https://github.com/stadt-karlsruhe/svg2imgmap#egg=svg2imgmap
+
+## Usage
+
+    $ svg2imgmap --help
+    usage: svg2imgmap.py [-h] [--layer LAYER] SVG WIDTH HEIGHT
+
+    Create an HTML image map from an SVG image.
+
+    <path> elements from the SVG are converted into <area> elements for the
+    image map. The path's `id` is used for the area's `id` and `href`, and
+    if the path has an Inkscape label then that is used for the area's `alt`
+    text.
+
+    This tool does not generate the background image for the image map, only
+    the HTML code for the <map> element. The coordinate calculations assume
+    that the whole SVG page is used for the background.
+
+    The generated HTML code is printed to STDOUT.
+
+    positional arguments:
+      SVG                   SVG file
+      WIDTH                 Width of the image map in pixels
+      HEIGHT                Height of the image map in pixels
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --layer LAYER, -l LAYER
+                            Use only the layer with this Inkscape label (can be
+                            specified multiple times)
+
+## License
+
+Copyright (c) 2016, Stadt Karlsruhe (www.karlsruhe.de)
+
+Distributed under the MIT license, see the file `LICENSE` for details.
+
--- svg2imgmap/__init__.py
+++ svg2imgmap/__init__.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright (c) 2016, Stadt Karlsruhe (www.karlsruhe.de)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 '''
 Create a HTML image map from an SVG image.
@@ -16,6 +37,9 @@ import textwrap
 
 from lxml import etree
 import svg.path
+
+
+__version__ = '0.1.0'
 
 
 SVG_NS = '{http://www.w3.org/2000/svg}'
@@ -385,8 +409,9 @@ def main():
     if the path has an Inkscape label then that is used for the area's `alt`
     text.
 
-    The coordinate calculation assume that the whole SVG page has been exported
-    to the given raster image.
+    This tool does not generate the background image for the image map, only
+    the HTML code for the <map> element. The coordinate calculations assume
+    that the whole SVG page is used for the background.
 
     The generated HTML code is printed to STDOUT.
     ''')
